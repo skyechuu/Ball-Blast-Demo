e7fb7db3e1e68942b168a0dfbdf900b75f7983af
Gokhan Nas <n.gokhan36@gmail.com>
2019-05-27 22:02:43 +0300

    Ball bounce and Ball spawner

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Ball : MonoBehaviour
+{
+    [SerializeField] float bounceHeight = 8f;
+    [SerializeField] float horizontalSpeed = 1f;
+    [SerializeField] int hitPoint = 1;
+
+    float gravity = -9.8f;
+    float velocityY;
+    float velocityX;
+    bool isLeft = false;
+    bool isBallReady = false;
+
+    void Awake()
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    void Start()
+    {
+        // TODO Set values from config file
+        gravity = -9.8f;
+    }
+    
+    void Update()
+    {
+#if UNITY_EDITOR
+        if (Input.GetKeyDown(KeyCode.Space))
+            Bounce();
+#endif
+
+        CheckBallReadiness();
+
+        Move();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Ground":
+                Bounce();
+                break;
+            case "Wall_R":
+                SidewayBounce(true);
+                break;
+            case "Wall_L":
+                SidewayBounce(false);
+                break;
+        }
+    }
+
+    void Move()
+    {
+        // Calcualte horizontal velocity
+        velocityX = horizontalSpeed * ((isLeft) ? -1 : 1);
+
+        if (isBallReady)
+        {
+            // Add gravity to vertical velocity
+            velocityY += gravity * Time.deltaTime;
+        }
+
+
+        // Move the ball
+        transform.position += (Vector3.up * velocityY + transform.right * velocityX) * Time.deltaTime;
+    }
+
+    /// <summary>
+    /// Bounces ball by calculating kinematic equation with gravity and desired bounceHeight and calls corresponding callback function at the end.
+    /// </summary>
+    void Bounce()
+    {
+        float bounceVel = Mathf.Sqrt(-2 * gravity * bounceHeight);
+        velocityY = bounceVel;
+
+        OnBounce();
+    }
+
+    /// <summary>
+    /// Changes direciton of ball's horizontal velocity and calls corresponding callback function at the end.
+    /// </summary>
+    /// <param name="newIsLeft">New state of isLeft variable as boolean.</param>
+    void SidewayBounce(bool newIsLeft)
+    {
+        isLeft = newIsLeft;
+
+        OnSidewayBounce();
+    }
+
+    void OnBounce()
+    {
+        // callback for sound, gfx, etc...
+        //Debug.Log("Boink!");
+    }
+
+    void OnSidewayBounce()
+    {
+        // callback for sound, gfx, etc...
+        //Debug.Log("Boink!");
+    }
+
+    /// <summary>
+    /// Check if the ball ready to drop and bounce to platform after it spawned. Prevents hitting the side walls.
+    /// </summary>
+    void CheckBallReadiness()
+    {
+        if (isBallReady)
+            return;
+
+        float platformLeft = -3.5f;
+        float platformRight = 3.5f;
+        Vector3 pos = transform.position;
+        Vector3 size = transform.localScale;
+
+        isBallReady = (pos.x >= platformLeft + size.x) && (pos.x <= platformRight - size.x);
+    }
+
+    /// <summary>
+    /// Sets direction of horizontal velocity.
+    /// </summary>
+    /// <param name="direction">Set 'true' for Left, 'false' for Right.</param>
+    public void SetIsLeft(bool direction)
+    {
+        isLeft = direction;
+    }
+
+    public void SetBounceHeight(float bounceHeight)
+    {
+        this.bounceHeight = bounceHeight;
+    }
+
+    public void SetHorizontalSpeed(float horizontalSpeed)
+    {
+        this.horizontalSpeed = horizontalSpeed;
+    }
+
+    public void SetHitPoint(int hitPoint)
+    {
+        this.hitPoint = hitPoint;
+    }
+
+}
---
+++ Assets/Scripts/BallSpawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawner : MonoBehaviour
+{
+    [SerializeField] int ballPreloadAmount = 7;
+    [SerializeField] GameObject ballPrefab;
+    
+    void Start()
+    {
+        SpawnBall(1, true, 1, 9);
+        SpawnBall(1, false, 2, 9);
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Spawns bouncing ball with given properties from side of the screen.
+    /// </summary>
+    /// <param name="initialDirection">Initial direction of horizontal velocity of ball. Set 'true' for Left, 'false' for Right.</param>
+    /// <param name="horizontalSpeed">Default 1 unity per second.</param>
+    /// <param name="bounceHeight">Default 8 unit, which is 1 unit less than height of the side walls.</param>
+    /// <param name="hitPoint">Hit point of the ball.</param>
+    Ball SpawnBall(int hitPoint = 1, bool initialDirection = false, float horizontalSpeed = 1f, float bounceHeight = 8f)
+    {
+        float positionX = 3.5f * (initialDirection ? 1 : -1);
+        Vector3 spawnPosition = new Vector3(positionX, transform.position.y, 0);
+        GameObject go = Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
+
+        Ball ball = go.GetComponent<Ball>();
+        ball.SetHitPoint(hitPoint);
+        ball.SetIsLeft(initialDirection);
+        ball.SetHorizontalSpeed(horizontalSpeed);
+        ball.SetBounceHeight(bounceHeight);
+        ball.gameObject.SetActive(true);
+
+        return ball;
+    }
+
+    /// <summary>
+    /// Spawns bouncing ball with given properties from given Ball position.
+    /// </summary>
+    /// <param name="parent">Parent ball that spawned the balls.</param>
+    /// <param name="initialDirection">Initial direction of horizontal velocity of ball. Set 'true' for Left, 'false' for Right.</param>
+    /// <param name="horizontalSpeed">Default 1 unity per second.</param>
+    /// <param name="bounceHeight">Default 9 unit, which is also size of the side walls.</param>
+    /// <param name="hitPoint">Hit point of the ball.</param>
+    Ball SpawnBall(Ball parent, int hitPoint = 1, bool initialDirection = false, float horizontalSpeed = 1f, float bounceHeight = 9f)
+    {
+        Vector3 spawnPosition =  parent.transform.position;
+        GameObject go = Instantiate(ballPrefab, spawnPosition, Quaternion.identity);
+
+        Ball ball = go.GetComponent<Ball>();
+        ball.SetHitPoint(hitPoint);
+        ball.SetIsLeft(initialDirection);
+        ball.SetHorizontalSpeed(horizontalSpeed);
+        ball.SetBounceHeight(bounceHeight);
+        ball.gameObject.SetActive(true);
+
+        return ball;
+    }
+
+}
